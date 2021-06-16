16a264113366d192342966a98b897fb73b5280a8
khanmo97 <1Whatisit?>
2021-06-16 15:32:44 -0500

    Finished up, needs a review before submission

---
+++ src/server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const bodyParser = require( 'body-parser' )
+const app = express()
+
+app.use( function ( req, res, next ) {
+	res.setHeader( 'Access-Control-Allow-Origin', 'http://localhost:3000' );
+	res.setHeader( 'Access-Control-Allow-Methods', 'POST' );
+	res.setHeader( 'Access-Control-Allow-Headers', 'Content-Type' );
+	next();
+});
+app.use( ( req, res, next ) => {
+	if ( !req.is( 'json' ) ) {
+		res.sendStatus( 415 );
+	} else {
+		next();
+	}
+});
+app.use( bodyParser.json() )
+app.post('/test', (req, res, next) => {
+	if (!req.body.hasOwnProperty('string_to_cut')) {
+		next({
+			expose: true,
+			statusCode: 400,
+			status: 400,
+			body: req.body,
+			type: "missing.property.string_to_cut"
+		});
+	} else {
+		let return_string = "";
+		for (i = 2; i < req.body.string_to_cut.length; i+=3)
+		{
+			return_string += req.body.string_to_cut.charAt(i)
+		}
+
+		res.status(201);
+		res.send({return_string});
+	}
+});
+app.use(async (err, req, res, next) => {
+	res.status(400).send({ error: err });
+});
+
+module.exports = app
---
+++ tests/server.test.js
@@ -0,0 +1,46 @@
+const request = require('supertest')
+const app = require('../src/server')
+
+const testRequest = (description, reqObject, resObject, statusCode) => {
+	it(description, async () => {
+		const res = await request(app)
+			.post('/test')
+			.set({'Content-Type': 'application/json'})
+			.send(reqObject)
+		expect(res.statusCode).toEqual(statusCode)
+		expect(res.body).toMatchObject(resObject)
+	})
+}
+describe('API response test', () => {
+	testRequest('should return every 3rd character', {string_to_cut: '123456789'}, {return_string: '369'}, 201);
+	testRequest('should allow empty string', {string_to_cut: ''}, {return_string: ''}, 201);
+	testRequest('should allow trailing characters', {string_to_cut: '12345678901'}, {return_string: '369'}, 201);
+	testRequest('should error for invalid object', 'notjson', {
+		error: {
+			expose: true,
+			statusCode: 400,
+			status: 400,
+			body: "notjson",
+			type: "entity.parse.failed"
+		}
+	}, 400);
+	testRequest('should error for missing string_to_cut property', {}, {
+		error: {
+			expose: true,
+			statusCode: 400,
+			status: 400,
+			body: {},
+			type: "missing.property.string_to_cut"
+		}
+	}, 400);
+});
+describe('Headers test', () => {
+	it('should have content type json', async () => {
+		const res = await request(app)
+			.post('/test')
+			.send('');
+		expect(res.statusCode).toEqual(415);
+		expect(res.body).toEqual({});
+	})
+});
+
